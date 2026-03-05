6c5aab5059a9dd5407315edf2f53f31e980cd896
daniloldn <112765545+daniloldn@users.noreply.github.com>
2026-03-05 15:52:12 +0000

    seeidng helper

---
+++ src/utils/seeding.py
@@ -0,0 +1,10 @@
+import os, random
+import numpy as np
+import tensorflow as tf
+
+def set_global_seed(seed: int):
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    os.environ["TF_DETERMINISTIC_OPS"] = "1"
+    random.seed(seed)
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
